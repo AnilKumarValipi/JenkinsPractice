4817491cc306bdc62ffa7a21fc2d8ed419cfab42
Anil Valipi <anilv808@gmail.com>
2025-08-26 14:59:59 +0530

    Initial commit: hello-ci with Gradle, tests, Jenkinsfile

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static String greet(String who) {
+        return "Hello, " + who + "!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet("World"));
+    }
+}
